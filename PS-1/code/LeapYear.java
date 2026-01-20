f34d71e01421627eab611685ad73970144ba5c30
Mayank jha <143251117+MAYANK2862@users.noreply.github.com>
2026-01-20 06:33:39 +0530

    Add LeapYear class to check leap years

---
+++ PS-1/code/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear{
+    public static void main(String[]args){
+         int year=2020;
+         
+         if (year%400==0){
+         System.out.println("Leap year");
+    }
+         else if( year%4==0 && year%100!=0){
+             System.out.println("Leap year");
+            
+         }
+         else{
+             System.out.println("Not a leap year");
+         }
+    }
+    
+}
